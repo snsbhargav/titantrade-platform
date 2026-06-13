8cecab11a53359e9c3bf0063ec2d34f83128dc10
Naga Sai Bhargav Siddineni <snsbhargav@gmail.com>
2026-06-12 20:49:07 -0400

    Initial Spring Boot Setup for titantrade backend

---
+++ backend/TitanTrade/src/main/java/com/bhargav/TitanTrade/TitanTradeApplication.java
@@ -0,0 +1,13 @@
+package com.bhargav.TitanTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanTradeApplication.class, args);
+	}
+
+}
