adfb75dccfd470136c42b436ac48df6884b90045
Naga Sai Bhargav Siddineni <snsbhargav@gmail.com>
2026-06-28 18:33:36 -0400

    Add portfolioHolding entity and repository

---
+++ backend/titantrade/src/main/java/com/bhargav/titantrade/portfolio/entity/PortfolioHolding.java
@@ -0,0 +1,49 @@
+package com.bhargav.titantrade.portfolio.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.bhargav.titantrade.stock.entity.Stock;
+import com.bhargav.titantrade.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "portfolio_holdings", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "stock_id" }) })
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PortfolioHolding {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "stock_id", nullable = false)
+	private Stock stock;
+	@Column(nullable = false)
+	private BigDecimal averageBuyPrice;
+	@Column(nullable = false)
+	private BigDecimal quantity;
+	private LocalDateTime createdOn;
+	private LocalDateTime updatedOn;
+
+}
---
+++ backend/titantrade/src/main/java/com/bhargav/titantrade/portfolio/repository/PortfolioHoldingRepository.java
@@ -0,0 +1,19 @@
+package com.bhargav.titantrade.portfolio.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bhargav.titantrade.portfolio.entity.PortfolioHolding;
+
+@Repository
+public interface PortfolioHoldingRepository extends JpaRepository<PortfolioHolding, UUID>{
+	
+	List<PortfolioHolding> findByUserId(UUID userId);
+	
+	Optional<PortfolioHolding> findByUserIdAndStockId(UUID userId, UUID stockId);
+
+}
