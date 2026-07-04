c3ceb77a9db77a7fe9940d5e67b28b9905b428f0
Naga Sai Bhargav Siddineni <snsbhargav@gmail.com>
2026-07-04 17:45:27 -0400

    Added field names for method validation exception

--- backend/titantrade/src/main/java/com/bhargav/titantrade/common/exception/GlobalExceptionHandler.java
+++ backend/titantrade/src/main/java/com/bhargav/titantrade/common/exception/GlobalExceptionHandler.java
@@ -1,23 +1,25 @@
 package com.bhargav.titantrade.common.exception;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.bhargav.titantrade.common.response.ApiResponse;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ApiResponse> validationExceptionHandler(MethodArgumentNotValidException ex) {
-		ArrayList<String> errors = new ArrayList<>();
-		for (ObjectError i : ex.getBindingResult().getAllErrors()) {
-			errors.add(i.getDefaultMessage());
+		Map<String, String> errors = new HashMap<>();
+		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+			errors.put(error.getField(), error.getDefaultMessage());
 		}
 		return new ResponseEntity<ApiResponse>(new ApiResponse(false, "Validation failed", errors),
 				HttpStatus.BAD_REQUEST);
@@ -51,12 +53,12 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(StockNotFoundException.class)
 	public ResponseEntity<ApiResponse> stockNotFoundException(StockNotFoundException ex){
-		return new ResponseEntity<ApiResponse>(new ApiResponse(false, ex.getMessage(), null), HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<ApiResponse>(new ApiResponse(false, ex.getMessage(), null), HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(PortfolioHoldingNotFoundException.class)
 	public ResponseEntity<ApiResponse> portfolioHoldingNotFoundException(PortfolioHoldingNotFoundException ex){
-		return new ResponseEntity<ApiResponse>(new ApiResponse(false, ex.getMessage(), null), HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<ApiResponse>(new ApiResponse(false, ex.getMessage(), null), HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(InsufficientHoldingQuantityException.class)
