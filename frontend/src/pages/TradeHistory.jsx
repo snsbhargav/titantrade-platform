6204457518aef5945eb7ed434afbcd4d05616639
Naga Sai Bhargav Siddineni <snsbhargav@gmail.com>
2026-07-15 20:48:28 -0400

    Add pagination and trade type filter

--- frontend/src/pages/TradeHistory.jsx
+++ frontend/src/pages/TradeHistory.jsx
@@ -5,19 +5,34 @@ function TradeHistory(){
 
     const [tradesList, setTradesList] = useState([]);
     const [message, setMessage] = useState("");
+    const [page, setPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [last, setLast] = useState(false);
+    const [tradeType, setTradeType] = useState("");
 
-    useEffect(() => {
-        const getTrades = async() => {
-            try{
-                const response = await api.get("/trades");
-                setTradesList(response?.data?.data?.trades || []);
-                setMessage(response?.data?.message || "Trades History retrieved successful");
-            } catch(error){
-                setMessage(error?.response?.data?.message || "Trade history retrieval failed");
-            }
+    const getTrades = async() => {
+        try{
+            let url = `/trades?page=${page}&size=10`;
+            if(tradeType)
+                url = url+`&tradeType=${tradeType}`;
+            const response = await api.get(url);
+            setTradesList(response?.data?.data?.trades || []);
+            setTotalPages(response?.data?.data?.totalPages);
+            setLast(response?.data?.data?.last);
+            setMessage(response?.data?.message || "Trades History retrieved successful");
+        } catch(error){
+            setMessage(error?.response?.data?.message || "Trade history retrieval failed");
         }
+    }
+    useEffect(() => {
+        
         getTrades();
-    }, []);
+    }, [page, tradeType]);
+
+    const handleTradeType = (event) => {
+        setTradeType(event.target.value);
+        setPage(0);
+    }
     return (
         <div>
             {!tradesList && <p>Loading Trade History...</p>}
@@ -25,18 +40,23 @@ function TradeHistory(){
             {tradesList && tradesList.length != 0 &&
             <>
                 <h1>Trade History</h1>
+                <select name="tradetype" value={tradeType} id="tradetype" onChange={handleTradeType}>
+                    <option value="">ALL</option>
+                    <option value="BUY">BUY</option>
+                    <option value="SELL">SELL</option>
+                </select>
                 <table border="1">
                     <thead>
                         <tr>
-                            <td>Stock Id</td>
-                            <td>Ticker</td>
-                            <td>Company Name</td>
-                            <td>Trade Type</td>
-                            <td>Quantity</td>
-                            <td>Price Per Share</td>
-                            <td>Total Amount</td>
-                            <td>Trade Status</td>
-                            <td>Executed At</td>
+                            <th>Stock Id</th>
+                            <th>Ticker</th>
+                            <th>Company Name</th>
+                            <th>Trade Type</th>
+                            <th>Quantity</th>
+                            <th>Price Per Share</th>
+                            <th>Total Amount</th>
+                            <th>Trade Status</th>
+                            <th>Executed At</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -59,6 +79,9 @@ function TradeHistory(){
                         }
                     </tbody>
                 </table>
+                <button name="prev" disabled={page===0}  onClick={() => {if(page!=0) setPage(page-1)}}>Prev</button>
+                <button name="next" disabled={last} onClick={()=>{if(!last) setPage(page+1)}}>Next</button>
+                <p>page {page+1} of {totalPages}</p>
             </>
             }
             {message && <p>{message}</p>}
